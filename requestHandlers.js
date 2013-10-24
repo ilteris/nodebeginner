dd4a42c75f91dc432849c74a783c8a2b6106f5a0
ilteris <ilteris@gmail.com>
2013-10-23 21:33:11 -0400

    add return to request handlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,11 @@
 function start() {
 	console.log("request handler for 'start' was called ");
+	return "Hello Start";
 }
 
 function upload() {
 	console.log("request handler for 'upload' was called");
+	return "Hello Upload";
 }
 
 exports.start  = start;
